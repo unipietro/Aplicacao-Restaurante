0d7113f027e2e66f728564c7516c68d6393d1c15
unipietro <64235362+unipietro@users.noreply.github.com>
2020-08-02 20:24:26 -0300

    Algumas Atualizações no Concept

---
+++ AppRestaurante/Clientes.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace AppRestaurante
+{
+    partial class Clientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClientesDTG = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientesDTG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ClientesDTG
+            // 
+            this.ClientesDTG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ClientesDTG.Location = new System.Drawing.Point(12, 46);
+            this.ClientesDTG.Name = "ClientesDTG";
+            this.ClientesDTG.Size = new System.Drawing.Size(494, 524);
+            this.ClientesDTG.TabIndex = 0;
+            this.ClientesDTG.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(998, 520);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Proximo Cliente";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(533, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NOME:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Clientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 582);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ClientesDTG);
+            this.Name = "Clientes";
+            this.Text = "Clientes";
+            this.Load += new System.EventHandler(this.Clientes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ClientesDTG)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ClientesDTG;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ AppRestaurante/Clientes.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace AppRestaurante
+{
+    public partial class Clientes : Form
+    {
+        private MySqlDataAdapter da = new MySqlDataAdapter();
+        private BindingSource bindingSource = new BindingSource();
+
+        public Clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Clientes_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Conexao con = new Conexao();
+                adpt = new MySqlDataAdapter("SELECT id_cliente, nome AS Nome, telefone AS Telefone, celular AS Celular, cep AS CEP, uf AS UF, cidade AS Cidade, bairro AS Bairro, logradouro AS Logradouro, n AS Numero, complemento AS Complemento, observacoes AS Observacoes FROM tabClientes", con.conec);
+                MySqlCommandBuilder commandBuilder = new MySqlCommandBuilder(adpt);
+
+                DataTable dat = new DataTable();
+                adpt.Fill(dat);
+                bindingSource.DataSource = dat;
+                ClientesGridView.DataSource = bindingSource;
+                ClientesGridView.Columns["id_cliente"].Visible = false;
+                ClientesGridView.Columns["Nome"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                ClientesGridView.Columns["Nome"].MinimumWidth = 100;
+                ClientesGridView.Columns["Telefone"].Width = 90;
+                ClientesGridView.Columns["Celular"].Width = 90;
+                ClientesGridView.Columns["CEP"].Width = 70;
+                ClientesGridView.Columns["UF"].Width = 30;
+                ClientesGridView.Columns["Cidade"].Width = 100;
+                ClientesGridView.Columns["Bairro"].Width = 100;
+                ClientesGridView.Columns["Logradouro"].Width = 100;
+                ClientesGridView.Columns["Numero"].Width = 100;
+                ClientesGridView.Columns["Complemento"].Width = 100;
+                ClientesGridView.Columns["Observacoes"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                ClientesGridView.Columns["Observacoes"].MinimumWidth = 100;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Desculpe, mas ocorreu um erro ao buscar as informações no banco de dados.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
